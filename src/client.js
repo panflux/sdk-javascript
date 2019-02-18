6bd3a1fdbb1ce356fd046b285616e0b81bf15eff
Jasper van Herpt <jasper.van.herpt@omines.com>
2019-02-18 15:20:17 +0100

    Allow browsers to start login sequence

--- src/client.js
+++ src/client.js
@@ -18,8 +18,33 @@ import WebSocket from 'isomorphic-ws';
 import fetch from 'cross-fetch';
 import gql from 'graphql-tag';
 
+const STATE_TOKEN = 'panflux_token';
+
 const DEFAULT_TOKEN_URL = 'https://panflux.app/oauth/v2/token';
 const DEFAULT_AUTHORIZE_URL = 'https://panflux.app/oauth/v2/authorize';
+
+/**
+ * @param {number} len
+ * @return {string}
+ */
+function generateCSRF(len) {
+    const arr = new Uint8Array((len || 40) / 2);
+    window.crypto.getRandomValues(arr);
+    return Array.from(arr, (dec) => ('0' + dec.toString(16)).substr(-2)).join('');
+}
+
+/**
+ * This function should be chained to a fetch request Promise to process the response parameters
+ *
+ * @param {object} response
+ * @return {*}
+ */
+function validateResponse(response) {
+    if (response.status !== 200) {
+        throw Error(response.statusText);
+    }
+    return response.json();
+}
 
 /**
  * The Client class is the main encapsulation of a Panflux API client.
@@ -43,6 +68,7 @@ class Client extends EventEmitter {
         if (token) {
             this._token = Promise.resolve(token);
         }
+        this._resolving = false;
     }
 
     /**
@@ -50,27 +76,70 @@ class Client extends EventEmitter {
      *
      * @param {object} opts
      * @param {object?} token Optional cached token from previous session
-     * @return {module.Client}
+     * @return {Client}
      */
     static init(opts, token) {
-        return new Client(opts, token);
+        const client = new Client(opts, token);
+
+        // When a valid token is present return early
+        if (null != client.token) {
+            return client;
+        }
+
+        // If we are running inside a browser check for a code query parameter.
+        if (client._isBrowser()) {
+            const q = location.search.substring(1);
+            const o = {};
+            q.replace(/([^=&]+)=([^&]*)/g, (m, key, value) => {
+                o[decodeURIComponent(key)] = decodeURIComponent(value);
+            });
+            // we only accept requests with a code and a state
+            if (undefined === o.code || undefined === o.state) {
+                return client;
+            }
+            client._resolving = true;
+            client._verifyAuthResponse(o.code, o.state)
+                .then((token) => {
+                    client._resolving = false;
+                    client._token = Promise.resolve(token);
+                    return client;
+                })
+                .catch((err) => console.error(err));
+        }
+
+        return client;
     }
 
     /**
-     * Login the user and retrieve an access token.
+     * @return {Promise<object>}
      */
-    async loginFromBrowser() {
-        if (!this._opts.clientID) {
-            throw Error('ClientID options are required to use OAuth2 access_code grant');
+    async login() {
+        if (this._isBrowser()) {
+            return Promise.resolve(this._loginFromBrowser());
         }
-        const url = this._opts.authURL || DEFAULT_AUTHORIZE_URL;
-        const q = Object.entries({
-            response_type: 'code',
-            redirect_uri: location.href,
-            scope: this._opts.scope || '',
-            client_id: this._opts.clientID,
-        }).map(([key, val]) => `${encodeURIComponent(key)}=${encodeURIComponent(val)}`).join('&');
-        location.href = url + '?' + q;
+    }
+
+    /**
+     * @param {string} code
+     *
+     * @return {Promise<object>}
+     */
+    async requestToken(code) {
+        return fetch(this._opts.tokenURL || DEFAULT_TOKEN_URL, {
+            method: 'POST',
+            body: JSON.stringify({
+                grant_type: 'authorization_code',
+                client_id: this._opts.clientID,
+                client_secret: this._opts.clientSecret,
+                redirect_uri: location.origin, // TODO Find better way to provide return URI
+                code: code,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(validateResponse)
+            .then(this._returnToken);
     }
 
     /**
@@ -94,22 +163,8 @@ class Client extends EventEmitter {
                 'Content-Type': 'application/json',
             },
         })
-            .then((response) => {
-                if (response.status !== 200) {
-                    throw Error(response.statusText);
-                }
-                return response.json();
-            })
-            .then((token) => {
-                if (!token.edges || token.edges.length < 1) {
-                    throw Error('Returned token must reference edges to be used in API');
-                }
-
-                this._token = token;
-                this.emit('newToken', token);
-                return token;
-            })
-        ;
+            .then(validateResponse)
+            .then(this._returnToken);
     }
 
     /**
@@ -212,6 +267,90 @@ class Client extends EventEmitter {
     /** @return {token} */
     get token() {
         return this._token;
+    }
+
+    /** @return {boolean} */
+    get resolving() {
+        return this._resolving;
+    }
+
+    /**
+     * Login the user and retrieve an access request code.
+     *
+     * @private
+     *
+     * @return {object}
+     */
+    _loginFromBrowser() {
+        if (!this._opts.clientID) {
+            throw Error('ClientID options are required to use OAuth2 access_code grant');
+        }
+        const url = this._opts.authURL || DEFAULT_AUTHORIZE_URL;
+        const token = generateCSRF(16);
+        const q = Object.entries({
+            response_type: 'code',
+            redirect_uri: location.origin,
+            scope: this._opts.scope || '',
+            client_id: this._opts.clientID,
+            state: token,
+        }).map(([key, val]) => `${encodeURIComponent(key)}=${encodeURIComponent(val)}`).join('&');
+
+        // Store the token so we can later verify if we requested the token
+        if (undefined === window['localStorage']) {
+            console.error('No localStorage is present. State validation cannot be performed.');
+        } else {
+            window.localStorage.setItem(STATE_TOKEN, token);
+        }
+
+        location.href = url + '?' + q;
+        return {};
+    }
+
+    /**
+     * @param {string} code
+     * @param {string} state
+     *
+     * @private
+     */
+    async _verifyAuthResponse(code, state) {
+        if (undefined === window['localStorage']) {
+            console.error('No localStorage is present. State validation cannot be performed.');
+        } else {
+            const token = window.localStorage.getItem(STATE_TOKEN);
+            if (token !== state) {
+                throw new Error('Response state does not match request token');
+            }
+        }
+
+        // Request an access code
+        return this.requestToken(code);
+    }
+
+    /**
+     * This function should be chained to a fetch request Promise, behind the validateResponse so the
+     * valid token will be checked, stored and returned. Use this function in combination with .bind()
+     *
+     * @private
+     * @param {object} token
+     * @return {{edges}|*}
+     */
+    _returnToken(token) {
+        if (!token.edges || token.edges.length < 1) {
+            throw Error('Returned token must reference edges to be used in API');
+        }
+
+        this._token = token;
+        this.emit('newToken', token);
+        return token;
+    }
+
+
+    /**
+     * @return {boolean}
+     * @private
+     */
+    _isBrowser() {
+        return window !== undefined && typeof window === 'object';
     }
 }
 
