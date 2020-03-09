1dde3e1e549e130adb21311b75ba2680929568ef
Jasper van Herpt <jasper.van.herpt@omines.com>
2020-03-09 09:57:12 +0100

    Fix issue with unhandled Promise rejection (#48)
    * Make sure refreshing errors get catched and triggers new login
    
    * Also make sure the additional login action gets logged on failure
    
    So we do not create a new possible uncatched promise exception fault

--- src/client.js
+++ src/client.js
@@ -320,7 +320,10 @@ class Client extends EventEmitter {
 
         // set a refresh timeout
         this._refreshTimer = setTimeout(() => {
-            this.refreshToken(token);
+            this.refreshToken(token).catch((err) => {
+                console.error('Error when refreshing token:', err);
+                this.login().catch((err) => console.err(err));
+            });
         }, (token.expire_time - ((+new Date() - 60000) / 1000)) * 1000);
     }
 
