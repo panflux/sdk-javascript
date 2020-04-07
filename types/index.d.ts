0890701dbba42f19c38f42df156b390c55100c72
Jasper van Herpt <jasper.van.herpt@omines.com>
2020-04-07 15:16:11 +0200

    Add typings to package

---
+++ types/index.d.ts
@@ -0,0 +1,34 @@
+declare module "@panflux/sdk" {
+
+    interface ClientOptions {
+        authURL?: string;
+        tokenURL?: string;
+        state?: string;
+        sameWindow?: boolean;
+        returnURL?: string;
+    }
+
+    class Client extends EventEmitter {
+
+        constructor(opts: ClientOptions, token: string|any);
+
+        static init(opts: ClientOptions, token: string|any);
+    
+        async login(): Promise<any>;
+
+        async requestToken(code: string, returnUrl: string): Promise<any>;
+        async refreshToken(token: any): Promise<any>;
+        async authenticate(): Promise<any>;
+        async handleBrowserResult(result: string|any, returnUrl: string): Promise<boolean>
+
+        async connect(): Promise<ApolloLink>
+        async query(query: string, variables: any): Promise<any>;
+        async subscribe(query: string, nextCallback: (data: any) => void, errorCallback: (err: any) => void, completeCallback: Function): Promise<ZenObservable.Subscription>;
+        async getLink(): Promise<ApolloLink>;
+
+        token: any;
+        readonly resolving: boolean;
+        readonly hasValidToken: boolean;
+
+    }
+}
